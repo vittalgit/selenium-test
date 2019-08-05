14cb429556ba8e4a6ff14bcb47edc5625df9e1f5
Vittal Angadi <vittalewit@gmail.com>
2019-08-05 15:52:16 +0000

    testng file updated

--- src/main/java/MySel20Proj/MySel20Proj/fbClass.java
+++ src/main/java/MySel20Proj/MySel20Proj/fbClass.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-public class diffClass {
+public class fbClass {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe"); 
 		
@@ -14,18 +14,18 @@ public class diffClass {
 		
 	    WebDriver driver = new FirefoxDriver();
 	    
-	    String gm_url = "https://accounts.google.com/signin";
+	    String fb_url = "https://www.facebook.com/";
 	    
-	    driver.get(gm_url);
+	    driver.get(fb_url);
 	    
 	    System.out.println("Selenium Webdriver Script in Firefox browser using Gecko Driver");
 	    
-	    driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys("hellothere@gmail.com");;
-	    driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
-	    driver.findElement(By.xpath("//input[@name='password']")).sendKeys("password1");
+	    driver.findElement(By.xpath("//input[@id='email']")).sendKeys("fbaccount");;
+	    //driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+	    driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("password1");
+	  driver.findElement(By.xpath("//input[@type='submit']")).click();
 	    Thread.sleep(500);
 	    
 	    driver.close();
 	}	
-
 }
---
+++ src/test/java/com/google/tests/GoogleHomePageTest.java
@@ -0,0 +1,37 @@
+package com.google.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePageTest {
+	private WebDriver driver; 
+	String appURL = "https://www.google.com/";
+
+	@BeforeClass
+	public void testSetUp() {		
+		System.out.println("Selenium Webdriver Script in Firefox browser using Gecko Driver");
+		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe"); 
+		FirefoxOptions options = new FirefoxOptions();
+		options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		System.out.println("At TEST - verifyGooglePageTittle");
+		driver.navigate().to(appURL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Google");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		System.out.println("At AFTERCLASS");
+		driver.quit();
+	}
+}
