7cbbbed3a908ea42ff72e7106ca1fff7a929f917
Vittal Angadi <vittalewit@gmail.com>
2019-08-07 06:17:36 +0000

    Update GoogleHomePageTest.java

--- src/test/java/com/google/tests/GoogleHomePageTest.java
+++ src/test/java/com/google/tests/GoogleHomePageTest.java
@@ -1,12 +1,15 @@
 package com.google.tests;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions; 
 
 public class GoogleHomePageTest {
 	private WebDriver driver; 
@@ -15,10 +18,16 @@ public class GoogleHomePageTest {
 	@BeforeClass
 	public void testSetUp() {		
 		System.out.println("Selenium Webdriver Script in Firefox browser using Gecko Driver");
-		System.setProperty("webdriver.gecko.driver","/home/selenium/tools/geckodriver"); 
+		//System.setProperty("webdriver.gecko.driver","/home/selenium/tools/geckodriver"); 
 		//FirefoxOptions options = new FirefoxOptions();
 		//options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-		driver = new FirefoxDriver();
+		//driver = new FirefoxDriver();
+		String chromeDriverPath = "/home/selenium/tools/chromedriver.exe" ;
+	    System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors", "--silent");
+	    driver = new ChromeDriver(options);	
+		
 	}
 	
 	@Test
